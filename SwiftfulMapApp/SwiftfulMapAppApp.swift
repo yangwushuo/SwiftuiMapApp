bebbcb8a444b2a51de25649e1dfe13437d60b761
jasonyang <51365593+yangwushuo@users.noreply.github.com>
2024-04-13 21:25:46 +0800

    Initial Commit

---
+++ SwiftfulMapApp/SwiftfulMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulMapAppApp.swift
+//  SwiftfulMapApp
+//
+//  Created by Jason on 2024/4/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
